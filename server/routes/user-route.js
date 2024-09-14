1cdcd0c0a2f01293b24aa59ac89628f051ac08fd
Tushar Sahu <tusharsahu190702@gmail.com>
2024-09-14 16:40:29 +0530

    feat : address model add | update address route

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getUserById } = require("../controllers/user-controller");
+const {
+  getUserById,
+  updateAddress,
+} = require("../controllers/user-controller");
 
 const router = express.Router();
 
 router.get("/me", getUserById);
+
+router.put("/address/:userId", updateAddress);
 
 module.exports = router;
