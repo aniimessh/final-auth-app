eeda4c58a5338d7998839c305a0833208bf89f51
aniimesssh <animeshgupta2.002@gmail.com>
2024-09-20 12:42:05 +0530

    feat : siginup with skeleton finished

---
+++ client/redux/api/auth.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const sendOTPAPI = async (payload: string) => {
+  try {
+    const payloadData = {
+      email: payload,
+    };
+    const response = await axios.post(
+      `${process.env.EXPO_PUBLIC_API_URL}api/v1/auth/send-otp`,
+      payloadData
+    );
+    return response.data;
+  } catch (error) {
+    return { error: true, message: "Something went wrong!" };
+  }
+};
+
+export const verifyOTPAPI = async (payload: { email: string; otp: string }) => {
+  try {
+    const response = await axios.post(
+      `${process.env.EXPO_PUBLIC_API_URL}api/v1/auth/verify-otp`,
+      payload
+    );
+    return response.data;
+  } catch (error: any) {
+    return { error: true, message: error.message };
+  }
+};
